18e097f607a6018ad8d9a381c330e53db9c18fd4
umzi <ab5447612@gmail.com>
2024-06-12 19:45:22 +0300

    add: doxing

--- best_tile.py
+++ best_tile.py
@@ -1,13 +1,35 @@
 import os
-
 import numpy as np
 from pepeline import read, save, cvt_color, CvtType, best_tile
 import cv2
 from tqdm.contrib.concurrent import process_map, thread_map
 
-
 class BestTile:
-    def __init__(self, in_folder: str, out_folder: str, tile_size: int = 1024, process_type: str = "thread"):
+    """
+    Class for processing images to find and save the best tile with the highest Laplacian intensity.
+
+    Attributes:
+    - `in_folder` (str): Input folder containing images.
+    - `out_folder` (str): Output folder to save processed images.
+    - `tile_size` (int): Size of the tile to extract. Default is 1024.
+    - `process_type` (str): Type of processing ('thread', 'process', or 'sequential'). Default is 'thread'.
+    - `all_images` (list): List of all image filenames in the input folder.
+
+    Methods:
+    - `process(img_name: str)`: Process a single image to find and save the best tile.
+    - `run()`: Run the processing on all images using the specified processing type.
+    """
+
+    def __init__(self, in_folder: str, out_folder: str, tile_size: int = 512, process_type: str = "thread"):
+        """
+        Initialize the BestTile class.
+
+        Args:
+        - `in_folder` (str): Input folder containing images.
+        - `out_folder` (str): Output folder to save processed images.
+        - `tile_size` (int): Size of the tile to extract. Default is 512.
+        - `process_type` (str): Type of processing ('thread', 'process', or 'for'). Default is 'thread'.
+        """
         self.in_folder = in_folder
         self.out_folder = out_folder
         if not os.path.exists(out_folder):
@@ -16,10 +38,16 @@ class BestTile:
         self.all_images = os.listdir(in_folder)
         self.process_type = process_type
 
-    def process(self, img_name:str):
+    def process(self, img_name: str):
+        """
+        Process a single image to find and save the best tile.
+
+        Args:
+        - `img_name` (str): Name of the image file to process.
+        """
         img = read(os.path.join(self.in_folder, img_name), 1, 0)
         img_shape = img.shape
-        if img_shape[0]< self.tile_size or img_shape[1]<self.tile_size:
+        if img_shape[0] < self.tile_size or img_shape[1] < self.tile_size:
             return
         if img_shape[0] == self.tile_size or img_shape[1] == self.tile_size:
             save(img, os.path.join(self.out_folder, img_name))
@@ -31,6 +59,9 @@ class BestTile:
              os.path.join(self.out_folder, img_name))
 
     def run(self):
+        """
+        Run the processing on all images using the specified processing type.
+        """
         if self.process_type == "thread":
             thread_map(self.process, self.all_images)
         elif self.process_type == "process":

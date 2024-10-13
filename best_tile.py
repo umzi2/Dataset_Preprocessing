979e11cd061d225f20656a45e137ddcc1d681836
umzi <131161226+umzi2@users.noreply.github.com>
2024-10-13 20:30:20 +0300

    Update best_tile.py

--- best_tile.py
+++ best_tile.py
@@ -3,7 +3,9 @@ import numpy as np
 from pepeline import read, save, cvt_color, CvtType, best_tile, ImgColor, ImgFormat
 import cv2
 from tqdm.contrib.concurrent import process_map, thread_map
+from tqdm import tqdm
 from chainner_ext import resize, ResizeFilter
+
 
 
 class BestTile:
@@ -38,9 +40,13 @@ class BestTile:
         self.out_folder = out_folder
         if not os.path.exists(out_folder):
             os.makedirs(out_folder)
+        self.out_list = os.listdir(out_folder)
         self.tile_size = tile_size
         self.all_images = os.listdir(in_folder)
         self.process_type = process_type
+
+    def save_result(self,img,img_name)->None:
+        save(img,os.path.join(self.out_folder, img_name))
 
     def process(self, img_name: str):
         """
@@ -49,24 +55,28 @@ class BestTile:
         Args:
         - `img_name` (str): Name of the image file to process.
         """
+        if img_name in self.out_list:
+            return
         img = read(os.path.join(self.in_folder, img_name), ImgColor.RGB, ImgFormat.F32)
         img_shape = img.shape
         if img_shape[0] < self.tile_size or img_shape[1] < self.tile_size:
             return
-        if img_shape[0] == self.tile_size or img_shape[1] == self.tile_size:
-            save(img, os.path.join(self.out_folder, img_name))
-            return
         result_name = ".".join(img_name.split(".")[:-1]) + ".png"
+        if img_shape[0] == self.tile_size and img_shape[1] == self.tile_size:
+            self.save_result(img,result_name)
+            return
         img_gray = cvt_color(img, CvtType.RGB2GrayBt2020)
-        laplacian_abs = np.abs(cv2.Laplacian(img_gray, -1))
         if self.scale > 1:
-            laplacian_abs = resize(laplacian_abs, (img_shape[1] // self.scale, img_shape[0] // self.scale),
-                                   ResizeFilter.Box, False).squeeze()
-            left_up_cord = best_tile(laplacian_abs, self.tile_size // self.scale) * self.scale
-        else:
+            img_gray = resize(img_gray, (img_shape[1] // self.scale, img_shape[0] // self.scale),
+                                   ResizeFilter.Linear, False)
+            laplacian_abs = np.abs(cv2.Laplacian(img_gray, -1))
             left_up_cord = best_tile(laplacian_abs, self.tile_size // self.scale)
-        save(img[left_up_cord[0]:left_up_cord[0] + self.tile_size, left_up_cord[1]:left_up_cord[1] + self.tile_size],
-             os.path.join(self.out_folder, result_name))
+            left_up_cord = [index*self.scale for index in left_up_cord]
+        else:
+            laplacian_abs = np.abs(cv2.Laplacian(img_gray, -1))
+            left_up_cord = best_tile(laplacian_abs, self.tile_size )
+        img = img[left_up_cord[0]:left_up_cord[0] + self.tile_size, left_up_cord[1]:left_up_cord[1] + self.tile_size]
+        self.save_result(img,result_name)
 
     def run(self):
         """
@@ -77,5 +87,5 @@ class BestTile:
         elif self.process_type == "process":
             process_map(self.process, self.all_images)
         else:
-            for img_name in self.all_images:
+            for img_name in tqdm(self.all_images):
                 self.process(img_name)
